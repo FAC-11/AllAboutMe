cfe123b7fdba7083eb2504bb6533ea7069fd0e56
jen-harris <jennifer.louise.harris@gmail.com>
2017-09-29 11:42:54 +0100

    #1 added passwordmodul.js

---
+++ src/controllers/passwordModule.js
@@ -0,0 +1,24 @@
+'use strict';
+const crypto = require('crypto');
+const env2 = require('./config.env');
+
+module.exports = () => {
+  const secret = process.env.SECRET;
+  if (!secret || typeof secret !== 'string') {
+    throw Error('invalid secret!');
+  }
+  const functions = {
+    sign: (value) => {
+      const hmac = crypto.createHmac('sha256', secret);
+      return hmac.update(value).digest('hex');
+    },
+    validate: (value, hash) => {
+      if (functions.sign(value) !== hash) {
+        return false;
+      } else {
+        return true;
+      }
+    }
+  };
+  return functions;
+}
