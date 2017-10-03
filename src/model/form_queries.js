04a7a2e05978993e05090c6cde3409c25833096f
Dakshina Scott <dakshina.scott@gmail.com>
2017-10-03 18:08:28 +0100

    #5 adds function to save aboutme section

--- src/model/form_queries.js
+++ src/model/form_queries.js
@@ -1,15 +1,22 @@
 const dbConnection = require('./database/db_connection');
 
 const formQueries = {
-  add: 'INSERT INTO about_me (user_id, likes, dislikes, \
-    strengths, weaknesses, uncomfortable, safe) VALUES \
-    ($1, $2, $3, $4, $5, $6, $7) RETURNING id',
   get: 'SELECT * FROM about_me WHERE user_id = $1',
-  update: 'UPDATE about_me SET (user_id, likes, dislikes, strengths, weaknesses, uncomfortable, safe) = ($1, $2, $3, $4, $5, $6, $7) WHERE user_id=$8'
+  update: 'UPDATE about_me SET (likes, dislikes, strengths, weaknesses, uncomfortable, safe) = ($1, $2, $3, $4, $5, $6) WHERE user_id=$7 RETURNING id',
 };
 
-const addAboutMe = (aboutMe) => {
-  const {user_id, likes, dislikes, strengths, weaknesses, uncomfortable, safe} = aboutMe;
-  return dbConnection.one(formQueries.add, [user_id, likes, dislikes, strengths, weaknesses, uncomfortable, safe])
-    .then( obj => obj.id );
+const saveAboutMe = (userId, aboutMeData) => {
+  const {
+    likes,
+    dislikes,
+    strengths,
+    weaknesses,
+    uncomfortable,
+    safe,
+  } = aboutMeData;
+  const aboutMeArr = [likes, dislikes, strengths, weaknesses, uncomfortable, safe, userId];
+  return dbConnection.one(formQueries.update, aboutMeArr)
+    .then(obj => obj.id);
 };
+
+module.exports = { saveAboutMe };
