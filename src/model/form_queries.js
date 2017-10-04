706072503ac43feaa2ebda5455e4358e7831745a
Dakshina Scott <dakshina.scott@gmail.com>
2017-10-04 11:56:20 +0100

    #5 Uses upsert query for about me table

--- src/model/form_queries.js
+++ src/model/form_queries.js
@@ -2,7 +2,12 @@ const dbConnection = require('./database/db_connection');
 
 const formQueries = {
   get: 'SELECT * FROM about_me WHERE user_id = $1',
-  update: 'UPDATE about_me SET (likes, dislikes, strengths, weaknesses, uncomfortable, safe) = ($1, $2, $3, $4, $5, $6) WHERE user_id=$7 RETURNING id',
+  update: 'INSERT INTO about_me \
+    (user_id, likes, dislikes, strengths, weaknesses, uncomfortable, safe) \
+    VALUES ($7, $1, $2, $3, $4, $5, $6) \
+    ON CONFLICT (user_id) \
+    DO UPDATE SET \
+    (likes, dislikes, strengths, weaknesses, uncomfortable, safe) = ($1, $2, $3, $4, $5, $6) RETURNING id',
 };
 
 const saveAboutMe = (userId, aboutMeData) => {
