e953dfd4814f8dfa6c3f04f62fa670cdd22fdbe8
Constantina <polyccon@gmail.com>
2017-10-05 11:32:21 +0100

    #5 query Generator now takes arguments from an object so it's more neat

--- src/model/queryGenerator.js
+++ src/model/queryGenerator.js
@@ -1,31 +1,21 @@
-// const update = 'INSERT INTO about_me (user_id, likes, dislikes, \
-//   strengths, weaknesses, uncomfortable, safe) VALUES \
-//   ($7, $1, $2, $3, $4, $5, $6) ON CONFLICT \
-//   (user_id) DO UPDATE SET (likes, dislikes, strengths, \
-//     weaknesses, uncomfortable, safe) = ($1, $2, $3, $4, $5, $6)',
-
-const aboutMe = ['user_id', 'likes', 'dislikes',
-  'strengths', 'weaknesses', 'uncomfortable', 'safe'];
-
-const symptoms = ['user_id', 'diagnosis', 'diagnosis_agreement',
-   'current_medication', 'therapies_helpful', 'keep_well'];
-
-const backgrounds = ['user_id', 'background'];
-
-const appointments = ['user_id', 'worker_preferences',
-  'appointment_preferences', 'parent_involved', 'email',
-  'mobile', 'telephone', 'contact_preference'];
-
-
-
-const queryGenerator = (array) => {
-  const dollars =  array.map( (item, index) => {
+const queryGenerator = (sectionName) => {
+  const sections = {
+    aboutMe: ['user_id', 'likes', 'dislikes',
+      'strengths', 'weaknesses', 'uncomfortable', 'safe'],
+    symptoms: ['user_id', 'diagnosis', 'diagnosis_agreement',
+      'current_medication', 'therapies_helpful', 'keep_well'],
+    backgrounds: ['user_id', 'background'],
+    appointments: ['user_id', 'worker_preferences',
+      'appointment_preferences', 'parent_involved', 'email',
+      'mobile', 'telephone', 'contact_preference'],
+  };
+  const dollars =  sections[sectionName].map( (item, index) => {
     return '$'+(index+1);
   });
 
-  const query = 'INSERT INTO about_me (' + array.join(', ')+') VALUES ('+ dollars.join(', ')+') ON CONFLICT (user_id) DO UPDATE SET ('+array.slice(1).join(', ')+') = ('+dollars.slice(1).join(', ')+')';
-
-  return query;
-}
+  return 'INSERT INTO about_me (' + sections[sectionName].join(', ')+') VALUES ('+
+    dollars.join(', ')+') ON CONFLICT (user_id) DO UPDATE SET ('+
+    sections[sectionName].slice(1).join(', ')+') = ('+dollars.slice(1).join(', ')+')';
+};
 
 module.exports = queryGenerator;
--- tests/database.test.js
+++ tests/database.test.js
@@ -85,13 +85,13 @@ test('Get user based on email', (t) => {
 //});
 
 test( 'queryGenerator', (t) => {
-  const array = ['user_id', 'likes', 'dislikes',
-    'strengths', 'weaknesses', 'uncomfortable', 'safe'];
 
- const expected ='INSERT INTO about_me (user_id, likes, dislikes, strengths, weaknesses, uncomfortable, safe) VALUES ($1, $2, $3, $4, $5, $6, $7) ON CONFLICT (user_id) DO UPDATE SET (likes, dislikes, strengths, weaknesses, uncomfortable, safe) = ($2, $3, $4, $5, $6, $7)';
+  const expected ='INSERT INTO about_me (user_id, likes, dislikes, strengths, weaknesses, uncomfortable, safe) VALUES ($1, $2, $3, $4, $5, $6, $7) ON CONFLICT (user_id) DO UPDATE SET (likes, dislikes, strengths, weaknesses, uncomfortable, safe) = ($2, $3, $4, $5, $6, $7)';
 
- const actual = queryGenerator(array);
+  const actual = queryGenerator('aboutMe');
 
   t.equal (actual, expected, 'should return correct sql query');
   t.end();
-})
+});
+
+test.onFinish( () => process.exit());
