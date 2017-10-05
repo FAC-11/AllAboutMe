e0c141cb59364e93c02c4c96710b6ad70b02b79a
Rebecacalvoquintero <rebecacalvoquintero@hotmail.es>
2017-10-05 23:31:50 +0100

    #1 #40 lock other pages from being rendered if the user has not logged in or signed up, added part of the progress bar

--- src/controllers/about.js
+++ src/controllers/about.js
@@ -1,3 +1,5 @@
+const requireLogin = require('./requireLogin');
+
 exports.get = (req, res) => {
   res.render('about', {
     activePage: { about: true },
--- src/controllers/requireLogin.js
+++ src/controllers/requireLogin.js
@@ -1,6 +1,6 @@
 const requireLogin = (req, res, next) => {
   if (!req.session.user_id) {
-    // redirects user to login page
+  // redirects user to login page
     req.session.destination = req.originalUrl;
     res.render('login', {
       pageTitle: 'Login',
