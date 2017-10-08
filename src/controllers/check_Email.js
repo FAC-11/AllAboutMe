6aab3889384a46a8c56a4948e4cc0e1ca0632b61
Rebecacalvoquintero <rebecacalvoquintero@hotmail.es>
2017-10-08 13:54:13 +0100

    email is sent

--- src/controllers/check_Email.js
+++ src/controllers/check_Email.js
@@ -1,6 +1,6 @@
 const checkEmail = (emailAddress) => {
   if (!emailAddress) {
-    throw new TypeError('Please enter an email address before sending the form');
+    throw new Error('Please enter an email address before sending the form');
   } else {
     return emailAddress;
   }
--- src/controllers/finish.js
+++ src/controllers/finish.js
@@ -1,5 +1,25 @@
+const checkEmail = require('./check_Email');
+const sendemail = require('sendemail');
+const env = require('env2')('config.env');
+
 exports.get = (req, res) => {
-  res.render('finish', {
-    activePage: {finish: true},
+  email('Hello', person, (error, result) => {
+    console.log(' - - - - - - - - - - - - - - - - - - - - -> email sent: ');
+    console.log(result);
+    console.log(' - - - - - - - - - - - - - - - - - - - - - - - - - - - -');
   });
-}
+  res.render('finish', {
+    activePage: {
+      finish: true,
+    },
+  });
+};
+
+const email = sendemail.email;
+sendemail.set_template_directory('src/email_templates');
+
+const person = {
+  name: 'Rebeca',
+  email: 'rebecacalvoquintero@hotmail.es',
+  subject: 'Hello :)',
+};
