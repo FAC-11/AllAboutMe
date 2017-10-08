293c38289d89b48bcb77c255b67f1ac765245e4c
Rebecacalvoquintero <rebecacalvoquintero@hotmail.es>
2017-10-08 21:00:05 +0100

    changed test

--- src/controllers/check_Email.js
+++ src/controllers/check_Email.js
@@ -1,8 +1,6 @@
 const checkEmail = (emailAddress) => {
   if (!emailAddress) {
     throw new Error('Please enter an email address before sending the form');
-  } else {
-    return emailAddress;
   }
 };
 
