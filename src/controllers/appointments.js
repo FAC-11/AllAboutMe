5f9866c4c6ffb28bd454dfd112dc594deadf5075
Dakshina Scott <dakshina.scott@gmail.com>
2017-10-10 12:42:37 +0100

    #53 save appointments section (missing hopes and concerns)

--- src/controllers/appointments.js
+++ src/controllers/appointments.js
@@ -1,7 +1,24 @@
+const { saveAppointments } = require('../model/form_queries');
+
 exports.get = (req, res) => {
   res.render('appointments', {
     activePage: { appointments: true },
     pageTitle: 'Your appointment',
-    percentage: '15%',
+  });
+};
+
+exports.post = (req, res) => {
+  saveAppointments(req.session.id, req.body).then(() => {
+    res.render('symptoms', {
+      activePage: { symptoms: true },
+      pageTitle: 'Symptoms & Difficulties',
+    });
+  }).catch((err) => {
+    console.log(err);
+    res.render('symptoms', {
+      activePage: { symptoms: true },
+      pageTitle: 'Symptoms & Difficulties',
+      messages: [{ error: true, message: 'Sorry - the appointments section couldn\'t be saved' }],
+    });
   });
 };
