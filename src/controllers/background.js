8aced434a3ecd682e200f8076588d74dfae66376
Dakshina Scott <dakshina.scott@gmail.com>
2017-10-11 11:31:16 +0100

    #53 Background section saves and prepopulates from database

--- src/controllers/background.js
+++ src/controllers/background.js
@@ -1,9 +1,47 @@
+const { saveBackgrounds, getSection } = require('../model/form_queries');
+
 exports.get = (req, res) => {
-  res.render('background', {
-    activePage: { background: true },
-    pageTitle: 'Your background',
-    percentage: '85%',
-    previousPage: '/about',
-    nextPage: '/send',
-  });
+  getSection(req.session.id, 'backgrounds')
+    .then((data) => {
+      res.render('background', {
+        activePage: { background: true },
+        pageTitle: 'Your background',
+        percentage: '85%',
+        previousPage: '/about',
+        nextPage: '/send',
+        background: data.background,
+      });
+    })
+    .catch((err) => {
+      console.log(err);
+      res.render('background', {
+        activePage: { background: true },
+        pageTitle: 'Your background',
+        percentage: '85%',
+        previousPage: '/about',
+        nextPage: '/send',
+      });
+    });
+};
+
+exports.post = (req, res) => {
+  saveBackgrounds(req.session.id, req.body)
+    .then(() => {
+      res.render('send', {
+        activePage: { send: true },
+        pageTitle: 'Send',
+        percentage: '85%',
+        previousPage: '/background',
+      });
+    })
+    .catch((err) => {
+      console.log(err);
+      res.render('send', {
+        activePage: { send: true },
+        pageTitle: 'Send',
+        percentage: '85%',
+        previousPage: '/background',
+        messages: [{ error: true, message: 'Sorry - the background section couldn\'t be saved' }],
+      });
+    });
 };
