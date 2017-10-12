2d14c6c5fd749153c8cf239e3b12fcb844cdfab2
Constantina <polyccon@gmail.com>
2017-10-12 15:37:33 +0100

    #6 user name added in email data

--- src/controllers/finish.js
+++ src/controllers/finish.js
@@ -4,43 +4,36 @@ const env = require('env2')('config.env');
 
 const email = sendemail.email;
 sendemail.set_template_directory('src/email_templates');
-const { getForm } = require('../model/form_queries');
-const { mergeObj, addData } = require('./helpers.js');
+const {getForm} = require('../model/form_queries');
+const {mergeObj, addData} = require('./helpers.js');
 
 exports.post = (req, res) => {
 
-  getForm(1)
-  //getForm(req.session.id)
-    .then((data) => {
+  getForm(req.session.id).then((data) => {
 
-      const person1 = {
-        name: 'person',
-        email: req.body.email,
-        subject: 'All about me questionnaire',
-      };
+    const person = {
+      name: req.session.user,
+      email: req.body.email,
+      subject: 'All about me questionnaire'
+    };
 
-      const cleanData = mergeObj(data);
-
-      console.log(cleanData);
-      addData(person1, cleanData);
-      console.log('person', person1);
-      return person1;
-    })
-    .catch( (error) => {
-      console.log('error', error);
-    })
-    .then( person => {
-      email('Hello', person, (error, result) => {
-        console.log(' - - - - - - - - - - - - - - - - - - - - -> email sent: ');
-        console.log(result);
-        console.log('error: ', error);
-        console.log('person', person);
-        console.log(' - - - - - - - - - - - - - - - - - - - - - - - - - - - -');
-      });
+    const cleanData = mergeObj(data);
+    addData(person, cleanData);
+    return person;
+  }).catch((error) => {
+    console.log('error', error);
+  }).then(person => {
+    email('Hello', person, (error, result) => {
+      console.log(' - - - - - - - - - - - - - - - - - - - - -> email sent: ');
+      console.log(result);
+      console.log('error: ', error);
+      console.log('person', person);
+      console.log(' - - - - - - - - - - - - - - - - - - - - - - - - - - - -');
     });
+  });
   res.render('finish', {
     activePage: {
-      finish: true,
-    },
+      finish: true
+    }
   });
 };
