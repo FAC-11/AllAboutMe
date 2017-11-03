4af9e47e3a100f0b68b2624ee9e43faef2342781
Dakshina Scott <dakshina.scott@gmail.com>
2017-11-03 17:47:30 +0000

    #2 Clicking theme button changes class on body

--- public/change_colour.js
+++ public/change_colour.js
@@ -1,12 +1,9 @@
-var pageTitle = document.getElementsByClassName('colourHeader')[0];
-var headerColour = document.getElementsByClassName('header-background-colour')[0];
-
 var yellowRedButtons = [].slice.call(document.querySelectorAll('.yellowRed'));
 
 yellowRedButtons.forEach(function(yellowRedButton){
   yellowRedButton.addEventListener('click', function () {
-    headerColour.style['background-color'] = "#EC0868";
-    pageTitle.style['color'] = '#FFBC0A';
+    document.body.classList = '';
+    document.body.classList.add('yellow-red');
   });
 });
 
@@ -14,8 +11,8 @@ var blueGreenButtons = [].slice.call(document.querySelectorAll('.blueGreen'));
 
 blueGreenButtons.forEach(function(blueGreenButton){
   blueGreenButton.addEventListener('click', function () {
-    headerColour.style['background-color'] = '#2E294E';
-    pageTitle.style['color'] = '#04A777';
+    document.body.classList = '';
+    document.body.classList.add('blue-green');
   });
 });
 
@@ -23,8 +20,8 @@ var turquoisePurpleButtons = [].slice.call(document.querySelectorAll('.turquoise
 
 turquoisePurpleButtons.forEach(function(turquoisePurpleButton){
   turquoisePurpleButton.addEventListener('click', function () {
-    headerColour.style['background-color'] = "rgba(18, 192, 191, 1)";
-    pageTitle.style['color'] = 'rgba(165, 84, 236, 1)';
+    document.body.classList = '';
+    document.body.classList.add('green-purple');
   });
 });
 
@@ -32,8 +29,8 @@ var yellowOrangeButtons = [].slice.call(document.querySelectorAll('.yellowOrange
 
 yellowOrangeButtons.forEach(function(yellowOrangeButton){
   yellowOrangeButton.addEventListener('click', function () {
-    headerColour.style['background-color'] = "#EA5015";
-    pageTitle.style['color'] = '#F70062';
+    document.body.classList = '';
+    document.body.classList.add('orange-yellow');
   });
 });
 
@@ -41,7 +38,7 @@ var pinkBlueButtons = [].slice.call(document.querySelectorAll('.pinkBlue'));
 
 pinkBlueButtons.forEach(function(pinkBlueButton){
   pinkBlueButton.addEventListener('click', function () {
-    headerColour.style['background-color'] = "#F73F8E";
-    pageTitle.style['color'] = '#0062A4';
+    document.body.classList = '';
+    document.body.classList.add('pink-blue');
   });
 });
