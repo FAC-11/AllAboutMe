56c7bd896eecc5a78dea3d35966d55ae32462345
Constantina <polyccon@gmail.com>
2017-11-08 18:09:38 +0000

    #106 better description for reset page test and travis.yml

--- tests/router.test.js
+++ tests/router.test.js
@@ -39,14 +39,14 @@ test('Forgot route', t => {
       t.end();
     });
 });
-test('Reset password route', t => {
+test('Reset password route with an expired token', t => {
   request(app)
     .get('/reset/509abfd5-0000-8ebb-c52f-afacca822733')
     .expect(302)
     .expect('Location', '/forgot')
     .expect('Content-Type', 'text/plain; charset=utf-8')
     .end((err, res) => {
-      t.equal(res.statusCode, 302, 'Status code is 302');
+      t.equal(res.statusCode, 302, 'Status code is 302 because it redirects to forgot page');
       t.error(err, 'No error');
       t.end();
     });
