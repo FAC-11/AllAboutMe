9e08448ad7c6795ecac9b9a59a529a5aeb377815
Constantina <polyccon@gmail.com>
2017-11-08 14:58:28 +0000

    #106 testing server responds with a 401 for restricted endpoints

--- tests/router.test.js
+++ tests/router.test.js
@@ -15,3 +15,76 @@ test('Home route (when not signed in)', t => {
       t.end();
     });
 });
+test('Restricted routes should respond with 401 when signed out', t => {
+  t.plan(10);
+  request(app)
+    .get('/home')
+    .expect(401)
+    .expect('Content-Type', /text\/html/)
+    .end((err, res) => {
+      t.equal(res.statusCode, 401, '/home');
+    });
+  request(app)
+    .get('/info_page')
+    .expect(401)
+    .expect('Content-Type', /text\/html/)
+    .end((err, res) => {
+     t.equal(res.statusCode, 401, '/info_page');
+    });
+  request(app)
+    .get('/about')
+    .expect(401)
+    .expect('Content-Type', /text\/html/)
+    .end((err, res) => {
+    t.equal(res.statusCode, 401, '/about');
+      });
+  request(app)
+    .get('/symptoms')
+    .expect(401)
+    .expect('Content-Type', /text\/html/)
+    .end((err, res) => {
+    t.equal(res.statusCode, 401, '/symptoms');
+    });
+  request(app)
+    .get('/appointments')
+    .expect(401)
+    .expect('Content-Type', /text\/html/)
+    .end((err, res) => {
+      t.equal(res.statusCode, 401, '/appointments');
+    });
+  request(app)
+    .get('/background')
+    .expect(401)
+    .expect('Content-Type', /text\/html/)
+    .end((err, res) => {
+    t.equal(res.statusCode, 401, '/background');
+    });
+  request(app)
+    .get('/colour_scheme')
+    .expect(401)
+    .expect('Content-Type', /text\/html/)
+    .end((err, res) => {
+    t.equal(res.statusCode, 401, '/colour_scheme');
+    });
+  request(app)
+    .get('/send')
+    .expect(401)
+    .expect('Content-Type', /text\/html/)
+    .end((err, res) => {
+     t.equal(res.statusCode, 401, '/send');
+    });
+  request(app)
+    .get('/progress')
+    .expect(401)
+    .expect('Content-Type', /text\/html/)
+    .end((err, res) => {
+    t.equal(res.statusCode, 401, '/progress');
+    });
+  request(app)
+    .get('/finish')
+    .expect(401)
+    .expect('Content-Type', /text\/html/)
+    .end((err, res) => {
+    t.equal(res.statusCode, 401, '/finish');
+    });
+});
