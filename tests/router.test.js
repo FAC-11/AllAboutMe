504b5907fa6cf9439b3d2f15fdd6b54a4ce80a3b
Constantina <polyccon@gmail.com>
2017-11-15 17:40:48 +0000

    #165 test for unknown route with status 404 rendering correct text

--- tests/router.test.js
+++ tests/router.test.js
@@ -47,6 +47,8 @@ test('Unknown route', t => {
     .expect('Content-Type', /text\/html/)
     .end((err, res) => {
       t.equal(res.statusCode, 404, 'Status code is 404');
+      t.error(err, 'No error');
+      t.ok(res.text.includes('Sorry! This page couldn&#x27;t be found.'), 'Unknown route responds with page containing \'Sorry! This page couldn&#x27;t be found.\' text');
       t.end();
     });
 });
