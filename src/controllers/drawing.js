fb80698ee95ac0f809be499a18999fdb63e3e17a
Dakshina Scott <dakshina.scott@gmail.com>
2017-11-16 15:12:22 +0000

    #10 removes brush type options

--- src/controllers/drawing.js
+++ src/controllers/drawing.js
@@ -4,7 +4,6 @@ const {
 } = require('../model/form_queries');
 
 exports.post = (req, res) => {
-  console.log(req.body);
   saveDrawing(req.session.id, req.body.fieldName, req.body.svg)
     .then(() => {
       console.log('saved');
