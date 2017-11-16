154beb6795302ca6169458e3798b2a0449192970
Constantina <polyccon@gmail.com>
2017-11-16 15:21:49 +0000

    #165 test logout redirects for authenticated routes -not formones

--- tests/router.test.js
+++ tests/router.test.js
@@ -139,7 +139,7 @@ test('GET authenticated routes', (t) => {
         // Then make requests to each authenticated route
         // setting cookie for each request
         request(app)
-          .get(`\\${page}`)
+          .get(`/${page}`)
           .set('Cookie', cookies)
           .expect(200)
           .end((err, res) => {
@@ -171,7 +171,7 @@ test('POST authenticated routes (form sections)', (t) => {
         // Then make requests to each authenticated route
         // setting cookie for each request
         request(app)
-          .post(`\\${page}`)
+          .post(`/${page}`)
           .set('Cookie', cookies)
           .expect(302)
           .end((err, res) => {
@@ -215,7 +215,45 @@ test('POST LOGOUT authenticated routes (form sections)', (t) => {
               .end((err, res) => {
                 t.equal(res.statusCode, 302, 'Status code should be 302 for redirect');
                 t.equal(res.header['location'], '/', 'Should redirect to /');
-                t.notEqual(res.header['location'], locationBefore, 'Logout button redirects from any page to login')
+                t.notEqual(res.header['location'], locationBefore, 'Logout button redirects from any page to login');
+              });
+          });
+      });
+    });
+});
+test('POST LOGOUT authenticated routes', (t) => {
+  const authenticatedPages = [
+    'home',
+    'progress',
+    'info-page',
+    'colour_scheme',
+    'send',
+    'finish',
+  ];
+  t.plan(authenticatedPages.length * 2);
+  // First login to get cookie
+  request(app)
+    .post('/login')
+    .type('form')
+    .send({ email: 'jam@gmail.com', password: 'password' })
+    .end((getCookieErr, loginRes) => {
+      const cookies = loginRes.headers['set-cookie'];
+      authenticatedPages.forEach((page) => {
+        // Then make requests to each authenticated route
+        // setting cookie for each request
+        request(app)
+          .get(`/${page}`)
+          .set('Cookie', cookies)
+          .expect(302)
+          .end((error, resB) => {
+
+            request(app)
+              .post('/logout')
+              .type('form')
+              .expect(302)
+              .end((err, res) => {
+                t.equal(res.statusCode, 302, 'Status code should be 302 for redirect');
+                t.equal(res.header['location'], '/', 'Should redirect to /');
               });
           });
       });
