02400af3296cf5a8fb00e008e6f13d5c5b6f031a
Dakshina Scott <dakshina.scott@gmail.com>
2017-11-17 11:12:53 +0000

    #10 removes locking functionality

--- public/draw.js
+++ public/draw.js
@@ -7,8 +7,17 @@
 
   fabric.Object.prototype.transparentCorners = false;
 
-  var drawingModeEl = $('drawing-mode'),
-    drawingOptionsEl = $('drawing-mode-options'),
+      canvas.setWidth = 500;
+  // make canvas responsive
+  window.addEventListener('resize', function() {
+    function resizeCanvas() {
+      var container = $('dual-input');
+      console.log(container.offsetWidth);
+    }
+    resizeCanvas();
+  }, true);
+
+    var drawingOptionsEl = $('drawing-mode-options'),
     drawingColorEl = $('drawing-color'),
     drawingLineWidthEl = $('drawing-line-width'),
     clearEl = $('clear-canvas'),
@@ -55,31 +64,6 @@
     e.preventDefault();
     canvas.clear()
   };
-
-  drawingModeEl.onclick = function(e) {
-    e.preventDefault();
-    imgEl = drawingModeEl.getElementsByTagName('img')[0];
-    canvas.isDrawingMode = !canvas.isDrawingMode;
-    if (canvas.isDrawingMode) {
-      imgEl.src = '/padlock-locked.png';
-      drawingOptionsEl.style.display = '';
-    }
-    else {
-      imgEl.src = '/padlock-unlocked.png';
-      drawingOptionsEl.style.display = 'none';
-    }
-  };
-
-  var brushOptions = document.getElementsByName('brush-options');
-  brushOptions.forEach(function(option) {
-    option.addEventListener('click', function(e) {
-      canvas.freeDrawingBrush = new fabric[this.value + 'Brush'](canvas);
-      if (canvas.freeDrawingBrush) {
-        canvas.freeDrawingBrush.color = drawingColorEl.value;
-        canvas.freeDrawingBrush.width = parseInt(drawingLineWidthEl.value, 10) || 1;
-      }
-    });
-  });
 
   drawingColorEl.onchange = function() {
     canvas.freeDrawingBrush.color = this.value;
