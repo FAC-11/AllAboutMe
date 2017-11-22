684e947353580eac3823c78c4c9b09f4613e087c
Constantina <polyccon@gmail.com>
2017-11-22 16:31:25 +0000

    #117 added functionality for hide button

---
+++ public/hide.js
@@ -0,0 +1,7 @@
+document.getElementById('arrowbutton').addEventListener('click', function(){
+  document.getElementById("backgroundtext").classList.toggle("clip");
+
+
+  document.getElementById("upbutton").classList.toggle("clip");
+  document.getElementById("downbutton").classList.toggle("clip");
+})
