45221d1e8d81c0c17e8f8ad18348f3c6630bddca
Dakshina Scott <dakshina.scott@gmail.com>
2017-11-24 13:28:16 +0000

    #10 ends /drawing responses

--- src/controllers/drawing.js
+++ src/controllers/drawing.js
@@ -6,7 +6,8 @@ const {
 exports.post = (req, res) => {
   saveDrawing(req.session.id, req.body.fieldName, req.body)
     .then(() => {
-        console.log('saved');
+      console.log('saved');
+      res.end();
     })
     .catch((err) => {
       console.log(err);
@@ -17,7 +18,7 @@ exports.get = (req, res) => {
   getDrawing(req.session.id, req.query.question + '_svg')
     .then((svgObj) => {
       res.setHeader('Content-Type', 'application/json');
-      res.send(svgObj);
+      res.end(svgObj);
     })
     .catch(console.log);
 };
