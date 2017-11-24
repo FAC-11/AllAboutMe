59e2f720c9a6ac984d55d0db7f999ce6f4296eb9
Dakshina Scott <dakshina.scott@gmail.com>
2017-11-24 16:03:39 +0000

    #10 debugging

--- public/draw.js
+++ public/draw.js
@@ -77,7 +77,7 @@
           width: canvas.width,
           height: canvas.height,
           svg: canvas.toSVG(),
-          //jpg: canvas.toDataURL({ format: 'jpeg' }),
+          jpg: canvas.toDataURL({ format: 'jpeg' }),
           fieldName: question + '_svg',
         };
         xhr.onreadystatechange = function() {
