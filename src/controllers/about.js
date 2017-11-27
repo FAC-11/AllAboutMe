9f0c8dd692d304dc317f48fb7ac9d669bb857367
Constantina <polyccon@gmail.com>
2017-11-27 21:06:58 +0000

    #111 error handling for get function appointments page

--- src/controllers/about.js
+++ src/controllers/about.js
@@ -1,48 +1,47 @@
-const { saveSection, getSection } = require('../model/form_queries');
+const {saveSection, getSection} = require('../model/form_queries');
 
 exports.get = (req, res) => {
-  getSection(req.session.id, 'about')
-    .then((data) => {
-      res.render('about', {
-        activePage: { about: true },
-        errorMessages: req.flash('error'),
-        successMessages: req.flash('success'),
-        pageTitle: 'About Me',
-        logoutButton: true,
-        progressPercentage: '50',
-        data,
-      });
-    })
-   .catch((err) => {
-      console.log(err);
-      req.flash('error', 'Sorry - we couldn\'t load your saved answers for this section');
-      res.render('about', {
-        activePage: { about: true },
-        errorMessages: req.flash('error'),
-        successMessages: req.flash('success'),
-        pageTitle: 'About Me',
-        logoutButton: true,
-        progressPercentage: '50',
-      });
+  getSection(req.session.id, 'about').then((data) => {
+    res.render('about', {
+      activePage: {
+        about: true,
+      },
+      errorMessages: req.flash('error'),
+      successMessages: req.flash('success'),
+      pageTitle: 'About Me',
+      logoutButton: true,
+      progressPercentage: '50',
+      data,
     });
+  }).catch((err) => {
+    console.log(err);
+    req.flash('error', 'Sorry - we couldn\'t load your saved answers for this section');
+    res.render('about', {
+      activePage: {
+        about: true,
+      },
+      errorMessages: req.flash('error'),
+      successMessages: req.flash('success'),
+      pageTitle: 'About Me',
+      logoutButton: true,
+      progressPercentage: '50',
+    });
+  });
 };
 
 exports.post = (req, res) => {
-  saveSection(req.session.id, 'about', req.body)
-    .then(() => {
-      const buttonPressed = req.body.button;
-      if (buttonPressed === 'next') {
-        res.redirect('background');
-      } else if (buttonPressed === 'previous') {
-        res.redirect('symptoms');
-      } else {
-        res.redirect('about');
-      }
-    })
-    .catch((err) => {
-      console.log(err);
-      req.flash('error', 'Sorry - the about me section couldn\'t be saved');
+  saveSection(req.session.id, 'about', req.body).then(() => {
+    const buttonPressed = req.body.button;
+    if (buttonPressed === 'next') {
+      res.redirect('background');
+    } else if (buttonPressed === 'previous') {
+      res.redirect('symptoms');
+    } else {
       res.redirect('about');
-    });
-
+    }
+  }).catch((err) => {
+    console.log(err);
+    req.flash('error', 'Sorry - the about me section couldn\'t be saved');
+    res.redirect('about');
+  });
 };
