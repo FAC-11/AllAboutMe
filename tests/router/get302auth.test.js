83406fcea9ebddfb2ebd06da760b6a4cc316bd50
Constantina <polyccon@gmail.com>
2017-11-28 16:02:57 +0000

    #111 #191 get functions auth routes redirect

---
+++ tests/router/get302auth.test.js
@@ -0,0 +1,18 @@
+const test = require('tape');
+const request = require('supertest');
+const app = require('../../src/app');
+
+test('Restricted routes should redirect to login when signed out', t => {
+  const routes = ['/home', '/info_page', '/about', '/symptoms', '/appointments', '/background', '/colour_scheme', '/send', '/progress', '/finish', '/additional_info'];
+  t.plan(routes.length * 2);
+  routes.forEach( (route) => {
+    request(app)
+      .get(route)
+      .expect(302)
+      .expect('Content-Type', 'text/plain; charset=utf-8')
+      .end((err, res) => {
+        t.equal(res.statusCode, 302, `${route} should redirect to login with status code 302`);
+        t.error(err, 'No error');
+     });
+  });
+});
