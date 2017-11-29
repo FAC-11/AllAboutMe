392ec8c6f12ed8ecf69f43699daaf1be37d677b1
Constantina <polyccon@gmail.com>
2017-11-29 15:54:23 +0000

    #111 #191 fixed bug with error showing on appointments when data is null

--- src/controllers/appointments.js
+++ src/controllers/appointments.js
@@ -1,24 +1,31 @@
-const {
-  saveSection,
-  getSection,
-} = require('../model/form_queries');
+const {saveSection, getSection} = require('../model/form_queries');
 
 exports.get = (req, res) => {
-  getSection(req.session.id, 'appointments')
-    .then((data) => {
-      // for ticking correct checkbox based on previously saved answer
-      const contactMethods = data.contact_preference ? data.contact_preference.replace(/\{|\}/g, '').split(',') : [];
+  getSection(req.session.id, 'appointments').then((data) => {
+    // for ticking correct checkbox based on previously saved answer
+    if (data) {
+      const contactMethods = data.contact_preference
+        ? data.contact_preference.replace(/\{|\}/g, '').split(',')
+        : [];
       let checked = {
         contactBy: {},
-        worker: { [data.gender_preference]: true },
-        time: { [data.time_preference]: true },
-        parent_involvement: { [data.parent_involvement]: true },
+        worker: {
+          [data.gender_preference]: true
+        },
+        time: {
+          [data.time_preference]: true
+        },
+        parent_involvement: {
+          [data.parent_involvement]: true
+        }
       };
       contactMethods.forEach((method) => {
         checked.contactBy[method] = true;
       });
       res.render('appointments', {
-        activePage: { appointments: true },
+        activePage: {
+          appointments: true
+        },
         errorMessages: req.flash('error'),
         successMessages: req.flash('success'),
         logoutButton: true,
@@ -26,35 +33,49 @@ exports.get = (req, res) => {
         data,
         checked,
         progressPercentage: '10',
-        firstPage: true,
+        firstPage: true
       });
-    })
-    .catch((err) => {
+    } else {
       req.flash('error', 'Sorry - we couldn\'t load your saved answers for this section');
       res.render('appointments', {
-        activePage: { appointments: true },
+        activePage: {
+          appointments: true
+        },
         errorMessages: req.flash('error'),
         successMessages: req.flash('success'),
         logoutButton: true,
         pageTitle: 'Your appointment',
         progressPercentage: '10',
-        firstPage: true,
+        firstPage: true
       });
+    }
+  }).catch((err) => {
+    req.flash('error', 'Sorry - we couldn\'t load your saved answers for this section');
+    res.render('appointments', {
+      activePage: {
+        appointments: true
+      },
+      errorMessages: req.flash('error'),
+      successMessages: req.flash('success'),
+      logoutButton: true,
+      pageTitle: 'Your appointment',
+      progressPercentage: '10',
+      firstPage: true
     });
+  });
 };
 
 exports.post = (req, res) => {
-  saveSection(req.session.id, 'appointments', req.body)
-    .then(() => {
-      const buttonPressed = req.body.button;
-      if (buttonPressed === 'next') {
-        res.redirect('symptoms');
-      } else {
-        res.redirect('appointments');
-      }
-    }).catch((err) => {
-      console.log(err);
-      req.flash('error', 'Sorry - the appointments section couldn\'t be saved');
+  saveSection(req.session.id, 'appointments', req.body).then(() => {
+    const buttonPressed = req.body.button;
+    if (buttonPressed === 'next') {
+      res.redirect('symptoms');
+    } else {
       res.redirect('appointments');
-    });
+    }
+  }).catch((err) => {
+    console.log(err);
+    req.flash('error', 'Sorry - the appointments section couldn\'t be saved');
+    res.redirect('appointments');
+  });
 };
