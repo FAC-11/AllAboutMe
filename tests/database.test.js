7b36ea559ff8be83ad09f5d4729c6ea05b41347f
Dakshina Scott <dakshina.scott@gmail.com>
2017-11-29 16:21:40 +0000

    #10 fixes tests to match updated database

--- tests/database.test.js
+++ tests/database.test.js
@@ -37,7 +37,7 @@ test('Get user from database', (t) => {
     id: 1,
     name: 'jam',
     email: 'jam@gmail.com',
-    password: '$2a$10$18rlZExmWYQMnN2nWkd69eMlA/hUDCcj7DcrI./MllCOUYcDYiwlK',
+    password: '$2a$10$CEicRuoB3hvCnlDx9Of/deXIiRInjoRhYuC9VKdox7n0zVXMbzJb2',
   };
 
   dbReset()
@@ -260,6 +260,15 @@ test('Get form from database', (t) => {
     likes_svg: JSON.stringify(escapedSvg),
     dislikes_svg: null,
     additional_info: null,
+    strengths_svg : null,
+    weaknesses_svg : null,
+    uncomfortable_svg : null,
+    safe_svg : null,
+    additional_info_svg : null,
+    concerns_svg : null,
+    hope_svg : null,
+    background_svg : null,
+    keep_well_svg : null,
   };
 
   dbReset()
