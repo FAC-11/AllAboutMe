b1c7970413d75738e8422493035df7584ce07efc
Constantina <polyccon@gmail.com>
2017-11-29 16:50:16 +0000

    #111 #191 added error handling additional info page

--- src/controllers/additional_info.js
+++ src/controllers/additional_info.js
@@ -1,47 +1,34 @@
-const { saveSection, getSection } = require('../model/form_queries');
+const {saveSection, getSection} = require('../model/form_queries');
 
-exports.get = (req, res) => {
-  getSection(req.session.id, 'additional')
-    .then((data) => {
-      res.render('additional_info', {
-        activePage: { additional_info: true },
-        errorMessages: req.flash('error'),
-        successMessages: req.flash('success'),
-        pageTitle: 'Additional Information',
-        logoutButton: true,
-        progressPercentage: '90',
-        data,
-      });
-    })
-    .catch((err) => {
-      console.log(err);
-      req.flash('error', 'Sorry - we couldn\'t load your saved answers for this section');
-      res.render('additional_info', {
-        activePage: { additional_info: true },
-        errorMessages: req.flash('error'),
-        successMessages: req.flash('success'),
-        pageTitle: 'Additional Information',
-        progressPercentage: '90',
-        logoutButton: true,
-      });
+exports.get = (req, res, next) => {
+  getSection(req.session.id, 'additional').then((data) => {
+    res.render('additional_info', {
+      activePage: {
+        additional_info: true
+      },
+      errorMessages: req.flash('error'),
+      successMessages: req.flash('success'),
+      pageTitle: 'Additional Information',
+      logoutButton: true,
+      progressPercentage: '90',
+      data
     });
+  }).catch((err) => {
+    next(err);
+  });
 };
 
-exports.post = (req, res) => {
-  saveSection(req.session.id, 'additional', req.body)
-    .then(() => {
-      const buttonPressed = req.body.button;
-      if (buttonPressed === 'next') {
-        res.redirect('send');
-      } else if (buttonPressed === 'previous') {
-        res.redirect('background');
-      } else {
-        res.redirect('additional_info');
-      }
-    })
-    .catch((err) => {
-      console.log(err);
-      req.flash('error', 'Sorry - the additional information section couldn\'t be saved');
+exports.post = (req, res, next) => {
+  saveSection(req.session.id, 'additional', req.body).then(() => {
+    const buttonPressed = req.body.button;
+    if (buttonPressed === 'next') {
+      res.redirect('send');
+    } else if (buttonPressed === 'previous') {
+      res.redirect('background');
+    } else {
       res.redirect('additional_info');
-    });
+    }
+  }).catch((err) => {
+    next(err);
+  });
 };
