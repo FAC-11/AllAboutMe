2e23dbad35ba7759296b77b8d4fef2f6cf7d2d04
Constantina <polyccon@gmail.com>
2017-11-29 16:56:38 +0000

    #111 #191 added error handling in post signup

--- src/controllers/signup.js
+++ src/controllers/signup.js
@@ -1,60 +1,48 @@
-const { hashPassword } = require('./helpers/password_module');
-const { getUser, addUser } = require('../model/user_queries');
-const { validateSignUp } = require('./helpers/validate');
-
+const {hashPassword} = require('./helpers/password_module');
+const {getUser, addUser} = require('../model/user_queries');
+const {validateSignUp} = require('./helpers/validate');
 
 exports.get = (req, res) => {
   res.render('signup', {
     errorMessages: req.flash('error'),
     successMessages: req.flash('success'),
     activePage: {
-      signup: true,
+      signup: true
     },
     pageTitle: 'Create an Account',
-    logoutButton: false,
+    logoutButton: false
   });
 };
 
-
-exports.post = (req, res) => {
+exports.post = (req, res, next) => {
   const userData = req.body;
   const validatedUser = validateSignUp(userData);
   if (!validatedUser.isValid) {
     req.flash('error', validatedUser.message);
     res.redirect('signup');
   } else {
-    getUser(userData.email)
-      .then((existingUser) => {
-        if (!existingUser) {
-          hashPassword(userData.password)
-            .then((hashedPassword) => {
-              return addUser(userData.name, userData.email, hashedPassword);
-            })
-            .then((id) => {
-              req.session.user = userData.name;
-              req.session.id = id;
-              res.redirect('/info_page');
-            })
-            .catch((err) => {
-              console.log(err);
-              res.status(500).render('error', {
-                layout: 'error',
-                statusCode: 500,
-                errorMessage: 'Internal server error',
-              });
-            });
-        } else {
-          req.flash('error', `Account already exists for ${userData.email}`);
-          res.redirect('signup');
-        }
-      })
-      .catch((err) => {
-        console.log(err);
-        res.status(500).render('error', {
-          layout: 'error',
-          statusCode: 500,
-          errorMessage: 'Internal server error',
+    getUser(userData.email).then((existingUser) => {
+      if (!existingUser) {
+        hashPassword(userData.password).then((hashedPassword) => {
+          return addUser(userData.name, userData.email, hashedPassword);
+        }).then((id) => {
+          req.session.user = userData.name;
+          req.session.id = id;
+          res.redirect('/info_page');
+        }).catch((err) => {
+          console.log(err);
+          res.status(500).render('error', {
+            layout: 'error',
+            statusCode: 500,
+            errorMessage: 'Internal server error'
+          });
         });
-      });
+      } else {
+        req.flash('error', `Account already exists for ${userData.email}`);
+        res.redirect('signup');
+      }
+    }).catch((err) => {
+      next(err);
+    });
   }
 };
