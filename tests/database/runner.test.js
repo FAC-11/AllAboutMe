986bf47f757645df1ac096915a2da18aac6ad0d6
Constantina <polyccon@gmail.com>
2017-11-29 19:10:56 +0000

    #111 #191 added runner.js file and package json test script

---
+++ tests/database/runner.test.js
@@ -0,0 +1,6 @@
+require('./form_queries/get_form.test');
+require('./form_queries/get_section.test');
+require('./form_queries/save_section.test');
+require('./user_queries/add_user.test');
+require('./user_queries/get_user.test');
+require('./user_queries/update_password.test');
