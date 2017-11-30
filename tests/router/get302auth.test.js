17bbcd90eb6fd1d6893caadf95d1141717cfcf5d
Constantina <polyccon@gmail.com>
2017-11-30 09:22:33 +0000

    #111 #191 test auth routes redirect to login

--- tests/router/get302auth.test.js
+++ tests/router/get302auth.test.js
@@ -3,16 +3,33 @@ const request = require('supertest');
 const app = require('../../src/app');
 
 test('Restricted routes should redirect to login when signed out', t => {
-  const routes = ['/home', '/info_page', '/about', '/symptoms', '/appointments', '/background', '/colour_scheme', '/send', '/progress', '/finish', '/additional_info'];
-  t.plan(routes.length * 2);
-  routes.forEach( (route) => {
+  const routes = [
+    '/home',
+    '/info_page',
+    '/about',
+    '/symptoms',
+    '/appointments',
+    '/background',
+    '/colour_scheme',
+    '/send',
+    '/progress',
+    '/finish',
+    '/additional_info',
+  ];
+  t.plan(routes.length * 3);
+  routes.forEach(route => {
     request(app)
       .get(route)
       .expect(302)
       .expect('Content-Type', 'text/plain; charset=utf-8')
       .end((err, res) => {
-        t.equal(res.statusCode, 302, `${route} should redirect to login with status code 302`);
+        t.equal(
+          res.statusCode,
+          302,
+          `${route} should redirect to login with status code 302`
+        );
+        t.equal(res.header['location'], '/login', `Should redirect to login`);
         t.error(err, 'No error');
-     });
+      });
   });
 });
