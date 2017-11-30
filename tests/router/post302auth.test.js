b135d85cd24b63b204f4222a530ed686f72e24db
Constantina <polyccon@gmail.com>
2017-11-30 09:12:50 +0000

    #111 #191 added a test for post auth pages redirect to correct location

--- tests/router/post302auth.test.js
+++ tests/router/post302auth.test.js
@@ -2,7 +2,7 @@ const test = require('tape');
 const request = require('supertest');
 const app = require('../../src/app');
 
-test('POST authenticated routes (form sections)', (t) => {
+test('POST authenticated routes (form sections)', t => {
   const authenticatedPages = [
     'about',
     'appointments',
@@ -10,7 +10,7 @@ test('POST authenticated routes (form sections)', (t) => {
     'symptoms',
     'additional_info',
   ];
-  t.plan(authenticatedPages.length*3);
+  t.plan(authenticatedPages.length * 4);
   // First login to get cookie
   request(app)
     .post('/login')
@@ -18,7 +18,7 @@ test('POST authenticated routes (form sections)', (t) => {
     .send({ email: 'jam@gmail.com', password: 'password' })
     .end((getCookieErr, loginRes) => {
       const cookies = loginRes.headers['set-cookie'];
-      authenticatedPages.forEach((page) => {
+      authenticatedPages.forEach(page => {
         // Then make requests to each authenticated route
         // setting cookie for each request
         request(app)
@@ -26,14 +26,23 @@ test('POST authenticated routes (form sections)', (t) => {
           .set('Cookie', cookies)
           .expect(302)
           .end((err, res) => {
-            t.equal(res.statusCode, 302, `${page} responds with 302 (redirects)`);
+            t.equal(
+              res.statusCode,
+              302,
+              `${page} responds with 302 (redirects)`
+            );
             t.ok(res.redirect, `${page} redirects`);
+            t.equal(
+              res.headers['location'],
+              `${page}`,
+              `should redirect to ${page} page for invalid email`
+            );
             t.error(err, 'No error');
           });
       });
     });
 });
-test('POST send page with invalid email', (t) => {
+test('POST send page with invalid email', t => {
   request(app)
     .post('/login')
     .type('form')
@@ -43,12 +52,20 @@ test('POST send page with invalid email', (t) => {
       request(app)
         .post('/send')
         .set('Cookie', cookies)
-        .send({ email: 'gmail.com'})
+        .send({ email: 'gmail.com' })
         .expect(302)
         .end((err, res) => {
-          t.equal(res.statusCode, 302, 'Send page responds with 302 (redirects)');
+          t.equal(
+            res.statusCode,
+            302,
+            'Send page responds with 302 (redirects)'
+          );
           t.ok(res.redirect, 'send page redirects for invalid email');
-          t.equal(res.headers['location'], 'send', 'should redirect to send page for invalid email')
+          t.equal(
+            res.headers['location'],
+            'send',
+            'should redirect to send page for invalid email'
+          );
           t.error(err, 'No error');
           t.end();
         });
